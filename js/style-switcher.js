3e9a86d299001080bf6a1f9c702b8a2fc29d9abb
NicolasAlberti <118451564+NicolasAlberti@users.noreply.github.com>
2023-07-29 00:58:04 -0300

    Add files via upload

---
+++ js/style-switcher.js
@@ -0,0 +1,43 @@
+/* ============== toggle style switcher ================== */
+
+const styleSwticherToggle = document.querySelector(".style-switcher-toggler");
+styleSwticherToggle.addEventListener("click", () => {
+  document.querySelector(".style-switcher").classList.toggle("open");
+});
+
+// esconder barra de troca de cor se der "SCROLL" na página
+
+window.addEventListener("scroll", () => {
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+});
+
+/* ============== theme colors ================== */
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+  alternateStyles.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+}
+
+/* ============== theme light and dark (tema escuro) ================== */
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
+window.addEventListener("load", () => {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+});
